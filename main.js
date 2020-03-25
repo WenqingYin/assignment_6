897f05a0afb7f5d60a859d9e8016d2acf1a0874e
WenqingYin <39289444+WenqingYin@users.noreply.github.com>
2020-03-25 12:52:56 -0400

    code finished, but the writeup still needs to be updated

--- main.js
+++ main.js
@@ -1,100 +1,125 @@
+//menu: click on one item, pops up according product detail page 
 
-
-//display product detail floating window 
-//commentted out because of some issues: product a is not clickable 
-/*
-let onClickItem=document.getElementsByClassName('product a');
-let floatingWindow=document.getElementById('original');
-let background=document.getElementById('bg');
-console.log(onClickItem);
-
-onClickItem.onclick=function(){
-	console.log('print');
-	floatingWindow.style.display='block';
-	background.style.display='block';
+//when the product is clicked, update the product detail page 
+var productList=document.getElementsByClassName('view_details');
+//console.log(productList);
+for (var i=0; i<productList.length; i++){
+	var product=productList[i];
+	//console.log(product);
+	console.log(product);
+	product.addEventListener('click',updateProductDetail);
 }
 
-*/
+localStorage.setItem('buttonClicked',false);
+console.log(localStorage);
 
+//update product detail page 
+function updateProductDetail(event){
+	var productDetailPage=document.getElementById('product_detail_page');
+	productDetailPage.style.display='block';
+	var button=event.target
+	var selectedProduct=button.parentElement;
 
+	//update the information on the floating window 
+	console.log(selectedProduct);
+	var newName=selectedProduct.getElementsByClassName('name')[0].innerHTML;
+	document.getElementsByClassName('product_title')[0].innerHTML=newName+' Cinnamon Roll';
+	var newImageSrc=selectedProduct.getElementsByClassName('pic')[0].src; 
+	document.getElementById('detail_image').src=newImageSrc; 
+	var newPrice=selectedProduct.getElementsByClassName('price')[0].innerHTML;
+	document.getElementsByClassName('pricing')[0].innerHTML=newPrice+'/ea'
 
-//get quantity and glazing as array 
-var quantity=document.getElementsByName('quantity');
-var glazing=document.getElementsByName('glazing');
+	//store the name and picture in local storage
+	localStorage.setItem('currName',newName);
+	localStorage.setItem('currPrice',newPrice);
+	localStorage.setItem('currImageSrc',newImageSrc);
 
-
-//make item an object 
-function itemProperty(name,quantity,glazing,price){
-	this.name=name;
-	this.quantity=quantity; 
-	this.glazing=glazing;
-	this.price=price; 
-}
-
-//when click on one item in the menu, make it an instance of itemProperty object 
-function createItem(){
-	var name="Original Cinnamon Roll";
-	var price=3.28; 
-	let itemOne=new itemProperty(name,0,'None',price); 
-	return itemOne; 
-}
-
-let itemOne=new itemProperty("Original Cinnamon Roll",0,'None',3.28);
-
-//check which button of quantity is cickd, update the data of item
-
-function checkQuantity(item){
-	if(item.checked){
-		itemOne.quantity=item.value;
+	//check which glazing is selected; add event listner to all of the options 
+	var glazingList=document.getElementsByClassName('selected_glazing');
+	for (var i=0; i<glazingList.length; i++){
+		var glazing=glazingList[i].getElementsByTagName('input')[0];
+		//console.log(glazing);
+		glazing.addEventListener('click',updateChoice);
+		//console.log(glazing);
 	}
-	console.log(itemOne.quantity); 
-	//update the value in the shopping cart page 
-	//document.getElementById("quantity-of-item").innerHTML="Quantity: "+item.value; 
-}
 
-
-//check which button of glazing is cickd, update the data of item
-function checkGlazing(item){
-	if (item.checked){
-		itemOne.glazing=item.value;
+	//check which quantity is selected; add event listner to all of the options 
+	var quantityList=document.getElementsByClassName('selected_quantity');
+	for (var i=0; i<quantityList.length; i++){
+		var quantity=quantityList[i].getElementsByTagName('input')[0];
+		quantity.addEventListener('click',updateChoice); 
+		//updateSubtotal
 	}
-	console.log(itemOne.glazing); 
+	
 }
 
-function subtotal(item){
-	var price=item.price;
-	var quantity=item.quantity;
-	var subtotal=price*quantity; 
-	return subtotal 
+function updateChoice(event){
+	//console.log(newPrice);
+	var choice=event.target; 
+	var valueOfChoice=choice.value;
+	console.log(valueOfChoice, typeof valueOfChoice);
+	console.log(parseInt(valueOfChoice))
+	var typeOfInput=choice.name; 
+	if (typeOfInput=='glazing'){
+		//store in local storage
+		localStorage.setItem('currGlazing',valueOfChoice);
+		//console.log(localStorage);
+	}
+	if (typeOfInput=='quantity'){
+		//update subtotal 
+		var quantity=parseInt(choice.value);
+		var priceString=document.getElementsByClassName('pricing')[0].innerHTML;
+		console.log(priceString);
+		var price=parseFloat(priceString.slice(1,5))
+		//console.log(price);
+		var newSubtotal=quantity*price;
+		console.log(newSubtotal);
+		//change the subtotal on display 
+		document.getElementById('subtotal').children[0].innerHTML='Subtotal: $'+newSubtotal;
+		//store in local storage
+		localStorage.setItem('currQuantity',valueOfChoice);
+		console.log(localStorage);
+		localStorage.setItem('currSubtotal',newSubtotal);
+	}
+
+	//add event listener to the add to cart button 
+	var addToCart=document.getElementById('add_to_cart');
+	addToCart.addEventListener('click', storeCurrProduct());
 }
-//update the subtotal
-function subtotalUpdate(){
-	var num=subtotal(itemOne);
-	document.querySelector('#total_price').innerHTML="Subtotal: $"+num;
-	//document.querySelector('#total_amount').innerHTML="Total:"+num;
-	console.log(itemOne.quantity);
+
+
+//when click add to cart button in the product deetail page, store it in the local storage 
+function storeCurrProduct(){
+	//check if the users selected both glazing and quantity 
+
+	var glazing=document.getElementsByName('glazing');
+	var quantity=document.getElementsByName('quantity');
+	var glazingChecked=false;
+	var quantityChecked=false;
+	for (i=0; i<glazing.length; i++){
+		if (glazing[i].checked){
+			console.log('yes glazing')
+			glazingChecked=true;
+		}
+	}
+	for (i=0; i<quantity.length; i++){
+		if (quantity[i].checked){
+			console.log('yes quantity')
+			quantityChecked=true;
+		}
+	}
+
+	//when 'add to cart' button is clicked 
+	if (glazingChecked==true && quantityChecked==true){
+		localStorage.setItem('buttonClicked',true);
+	}
+	/*
+	else {
+		alert("Hello! I am an alert box!!");
+	}
+	//console.log(localStorage);
+	*/
 }
 
-function displaySubtotal(){
-	document.querySelector('#subtotal-of-item').style.display=inline-block;
-}
-
-//when clicking add to cart, update the items in the shopping cart 
-
-addItem()
-function addItem(){
-	console.log('yes');
-	console.log(document.querySelector(".quantity-of-item"));
-	console.log(document.getElementsByClassName("quantity"));
-	console.log(itemOne[quantity]);
-	//first item
-	//update subtotal, total, quantity, glazing 
-	document.querySelector(".quantity-of-item").innerHTML="Quantity: "+itemOne.quantity; 
-	document.querySelector(".subtotal-of-item").innerHTML="Subtotal: "+subtotal(itemOne);  
-	document.querySelector(".flavor-of-item").innerHTML=itemOne.glazing;   
-	//update total
-	document.querySelector("#totalAmount").innerHTML="Total: "+subtotal(itemOne);
-}
-
-
+//localStorage.clear();
 
--- shopping_cart.js
+++ shopping_cart.js
@@ -1,14 +1,185 @@
-let productInCart=document.getElementById('item_in_cart');
-let deleteOption=document.getElementById('delete'); 
-console.log(productInCart);
+//checkout button: clear the items
+function clearItems(){
+	localStorage.clear();
+	console.log(localStorage);
+	var temp=JSON.parse(localStorage.getItem('cartItems'));
+	console.log(temp);
+	displayItems(temp);
+}
 
-productInCart.onmouseover=function(){
-	//let 'delete' appear
-	deleteOption.style.display='inline-block';
+//function constructor for the items in cart 
+function itemsInCart(name,imageSrc,quantity,glazing,subtotal){
+	this.name=name;
+	this.imageSrc=imageSrc;
+	this.quantity=quantity;
+	this.glazing=glazing;
+	this.subtotal=subtotal; 
+}
 
+
+//add objects to the list 
+
+//console.log(JSON.parse(localStorage.getItem('cartItems')));
+
+var addToCart=localStorage.getItem('buttonClicked');
+
+var currAllItems=JSON.parse(localStorage.getItem('cartItems'));
+displayItems(currAllItems);
+
+if (addToCart=='true'){
+	//declare variables 
+	var itemList=JSON.parse(localStorage.getItem('cartItems'));
+
+	var addedName=localStorage.getItem('currName');
+	var addedImageSrc=localStorage.getItem('currImageSrc');
+	var addedQuantity=parseInt(localStorage.getItem('currQuantity'));
+	var addedGlazing=localStorage.getItem('currGlazing');
+	var addedSubtotal=parseInt(localStorage.getItem('currSubtotal')); 
+	var addedPrice=localStorage.getItem('currPrice'); 
+
+	//console.log(localStorage);
+	//console.log(addedImageSrc);
+
+	//loop all items in the itemList, find if this item has already existed in the cart 
+	var alreadyAdded=false; 
+
+	//if there is no object in the cart 
+	if (itemList==null){
+		var tempList=[];
+		//set the first object
+		var tempItem=new itemsInCart(addedName,addedImageSrc,addedQuantity,addedGlazing,addedSubtotal);
+		tempList.push(tempItem);
+		//update storage
+		localStorage.setItem("cartItems",JSON.stringify(tempList));
+		itemList=tempList;
+		console.log(itemList);
+		displayItems(itemList);
+	}
+
+	else {
+
+		for (i=0; i<itemList.length; i++){
+			if (itemList[i].name==addedName&&itemList[i].glazing==addedGlazing){
+				//update the properties of the object 
+				itemList[i].quantity+=addedQuantity; 
+				itemList[i].subtotal+=addedSubtotal;
+				//update the local storage 
+				localStorage.setItem("cartItems",JSON.stringify(itemList));
+				alreadyAdded=true; 
+				break; 
+			}
+		}
+
+		if (alreadyAdded==false){
+		//add this item to the list 
+			var tempItem=new itemsInCart(addedName,addedImageSrc,addedQuantity,addedGlazing,addedSubtotal);
+			itemList.push(tempItem); 
+			//console.log(itemList);
+			//console.log(itemList.length);
+			//update the local storage 
+			localStorage.setItem("cartItems",JSON.stringify(itemList));
+		}
+		displayItems(itemList);
+	}
+	localStorage.setItem('buttonClicked',false);
+}
+
+
+//according to the item list in JSON storage, use loop to display all of the items on the page 
+function displayItems(itemList){
+	//first delete all previous items in the cart 
+	var cartProducts=document.getElementById('allItems');
+	cartProducts.innerHTML='';
+
+	var total=0; 
+
+	if (itemList!==null){
+		for (i=0; i<itemList.length; i++){
+			var newName=itemList[i].name; 
+			var newImageSrc=itemList[i].imageSrc;
+			var newGlazing=itemList[i].glazing;
+			var newSubtotal=itemList[i].subtotal; 
+			var newQuantity=itemList[i].quantity;
+			//add a div to the shopping cart page 
+			var newProduct=document.createElement('div');
+			newProduct.classList.add('item_in_cart');
+			var insideNewProduct=`
+						<div class='image'>
+							<img class="pic-of-item" src="${newImageSrc}" width=100 height=65>
+						</div>
+						<div class='name-of-item'>
+							<p>${newName}</p>
+						</div>
+						<div class="quantity-of-item">
+							<p>Quantity: ${newQuantity}</p>
+						</div>
+						<div class="delete">
+							<p>Delete</p>
+						</div>
+						<div class="subtotal-of-item">
+							<p>Subtotal: $${newSubtotal}</p>
+						</div>
+						<div class="flavor-of-item">
+							<p>${newGlazing}</p>
+						</div>`
+			newProduct.innerHTML=insideNewProduct; 
+			cartProducts.append(newProduct); 
+			total+=newSubtotal;
+			console.log(total);
+		}
+		var currAllItems=document.getElementsByClassName('item_in_cart');
+	}
+
+	//update total 
+	var totalAmount=document.getElementById('total_amount');
+	totalAmount.innerText='Total: $'+total;
+
+	//delete the object 
+	var deleteBtn=document.getElementsByClassName('delete'); 
+	console.log(deleteBtn);
+	for (i=0; i<deleteBtn.length; i++){
+		console.log('in the loop');
+		console.log(deleteBtn[i]);
+		var temp=deleteBtn[i];
+		temp.addEventListener('click',deleteItem);
+	}
 
 }
 
-productInCart.onmouseout=function(){
-	deleteOption.style.display='none';
+
+function deleteItem(event){
+	var item=event.target.parentElement.parentElement;
+	var deleteName=item.getElementsByClassName('name-of-item')[0].innerText; 
+	var deleteGlazing=item.getElementsByClassName('flavor-of-item')[0].innerText;
+	console.log(item);
+	console.log(deleteName);
+	console.log(deleteGlazing);
+	var currStorageItems=JSON.parse(localStorage.getItem('cartItems'));
+	console.log(currStorageItems);
+	//loop through the items, find the same product, and delete it 
+	for (i=0; i<currStorageItems.length; i++){
+		var currItem=currStorageItems[i];
+		if (currItem.name==deleteName && currItem.glazing==deleteGlazing){
+			currStorageItems.splice(i, 1);
+			console.log(currStorageItems);
+		}
+	}
+	//update storage 
+	localStorage.setItem("cartItems",JSON.stringify(currStorageItems));
+	displayItems(currStorageItems);
 }
+
+
+
+// mouseover the item, display the delete
+function displayDelete(currAllItems){
+	console.log(currAllItems);
+	for (i=0; i<currAllItems.length; i++){
+		var currItem=currAllItems[i];
+		console.log(currItem);
+		var deleteBtn=currItem.getEelementsByClassName('delete')[0];
+		deleteBtn.style.display='inline-block';
+	}
+
+}
+
